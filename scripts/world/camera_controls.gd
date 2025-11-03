2a91f775aafda31c5f349f4a11ebee511b08a2db
Armando Martinez <martinez.armando508w@gmail.com>
2025-11-02 23:33:57 -0700

    Add max zoom out and in to camera

--- scripts/world/camera_controls.gd
+++ scripts/world/camera_controls.gd
@@ -4,9 +4,13 @@ var _cam
 
 @export_range (2.0, 20, 1) var rotation_speed: float = 2.0
 @export_range (1.0, 30, 2) var movement_speed: float = 6.0
+
 @export var current_rotation: float = 0
 @export var camera_rotation_speed: float = 0.5
 @export var camera_rotation_amount: float = 1
+
+@export var camera_max_zoom_in: int = 1
+@export var camera_max_zoom_out: int = 50
 
 @export var snap := true
 
@@ -14,17 +18,16 @@ var camera_animation_running: bool = true
 var _in_focus: bool = true
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_cam = %Camera3D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	if Input.is_action_pressed("left") and _in_focus == true:
 		#rotation.y += rotation_speed * delta
 		_cam.position.x -= movement_speed * delta
+
+
 	if Input.is_action_pressed("right") and _in_focus == true:
 		#rotation.y -= rotation_speed * delta
 		_cam.position.x += movement_speed * delta
@@ -39,18 +42,23 @@ func _process(delta: float) -> void:
 		#rotation.x -= rotation_speed * delta
 		_cam.position.y -= movement_speed * delta
 
+
 	if Input.is_action_just_pressed("arrow_right") and _in_focus:
 		_rotate_camera(camera_rotation_amount)
 
 
 	if Input.is_action_just_pressed("arrow_left") and _in_focus:
 		_rotate_camera(-camera_rotation_amount)
-		
+
+
 	if Input.is_action_just_pressed("scroll_up") and _in_focus:
-		_cam.size -= 1
-		
+		if not _cam.size <= camera_max_zoom_in:
+			_cam.size -= 1
+
+
 	if Input.is_action_just_pressed("scroll_down") and _in_focus:
-		_cam.size += 1
+		if not _cam.size >= camera_max_zoom_out:
+			_cam.size += 1
 	
 
 func _rotate_camera(direction: float):

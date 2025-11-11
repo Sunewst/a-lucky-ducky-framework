a1458694275265bdd36608beaabbb842e1862249
Armando Martinez <martinez.armando508w@gmail.com>
2025-11-11 12:18:16 -0700

    Split pond and board saving functions

--- scripts/save/SaveHandler.gd
+++ scripts/save/SaveHandler.gd
@@ -5,21 +5,23 @@ func _ready() -> void:
 		DirAccess.make_dir_absolute("user://save_data")
 
 
-func save_flock():
-	var board_save: FileAccess = FileAccess.open("user://save_data//board.save", FileAccess.WRITE)
+func save_pond():
 	var pond_save: FileAccess = FileAccess.open("user://save_data//pond.save", FileAccess.WRITE)
-	
 	var pond_nodes = get_tree().get_nodes_in_group("World")
-	var code_editor_nodes = get_tree().get_nodes_in_group("CodeEditor")
-	
-	for code_editor in code_editor_nodes:
-		var node_data = code_editor.call("save")
-		var json_string = JSON.stringify(node_data)
-
-		board_save.store_line(json_string)
 	
 	for pond_node in pond_nodes:
 		var node_data = pond_node.call("save")
 		var json_string = JSON.stringify(node_data)
 
 		pond_save.store_line(json_string)
+
+
+func save_board_data():
+	var board_save: FileAccess = FileAccess.open("user://save_data//board.save", FileAccess.WRITE)
+	var code_editor_nodes = get_tree().get_nodes_in_group("CodeEditor")
+
+	for code_editor in code_editor_nodes:
+		var node_data = code_editor.call("save")
+		var json_string = JSON.stringify(node_data)
+
+		board_save.store_line(json_string)

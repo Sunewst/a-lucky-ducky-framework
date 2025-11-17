193e5287d235d6b448bbf51604adeb1d99fcff8a
Armando Martinez <martinez.armando508w@gmail.com>
2025-11-16 20:54:31 -0700

    Add new find functions
    Add find total functions
    Add find total data types
    Add get setup function location
    
    Also fix get loop location counting even if commented out

--- scripts/editor/code_editor_util.gd
+++ scripts/editor/code_editor_util.gd
@@ -17,5 +17,48 @@ static func find_total_occurrences(text: String, editor: CodeEdit) -> Array[Vect
 			break
 	return _occurences_locations
 
+
 static func get_loop_location(editor: CodeEdit) -> Vector2i:
-	return editor.search("Void loop()", 2, 0, 0)
+	var location = find_total_occurrences("void loop()", editor)
+	if not location.is_empty():
+		return location[0]
+	else:
+		return Vector2i(-1, -1)
+
+
+static func get_setup_location(editor: CodeEdit) -> Vector2i:
+	var location = find_total_occurrences("void setup()", editor)
+	if not location.is_empty():
+		return location[0]
+	else:
+		return Vector2i(-1, -1)
+
+
+static func find_total_data_types(editor: CodeEdit, data_types: Array[String]):
+	var _potential_data_types: Array[Vector2i]
+	var _occurences_locations: Array[Vector2i]
+
+	for data_type in data_types:
+		_potential_data_types.append_array(find_total_occurrences(data_type, editor))
+
+	for occurence in _potential_data_types:
+		var current_line = editor.get_line(occurence.y)
+		if not current_line.contains("("):
+			_occurences_locations.append(occurence)
+
+	return _occurences_locations
+
+
+static func find_total_functions(editor: CodeEdit, data_types: Array[String]):
+	var _occurences_locations: Array[Vector2i]
+	var _potential_functions: Array[Vector2i]
+
+	for data_type in data_types:
+		_potential_functions.append_array(find_total_occurrences(data_type, editor))
+
+	for data_type in _potential_functions:
+		var current_line = editor.get_line(data_type.y)
+		if current_line.contains("("):
+			_occurences_locations.append(data_type)
+
+	return _occurences_locations

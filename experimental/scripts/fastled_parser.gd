e3201dd6f9e016d59f1fd36ccb39da7e7afb0587
Armando Martinez <martinez.armando508w@gmail.com>
2025-11-16 23:31:49 -0700

    Add convert data types function

--- experimental/scripts/fastled_parser.gd
+++ experimental/scripts/fastled_parser.gd
@@ -23,13 +23,14 @@ static func parse_code(editor: CodeEdit):
 	var variable_locations = EditorHelper.find_total_data_types(editor, DATA_TYPES)
 	var functions_locations = EditorHelper.find_total_functions(editor, DATA_TYPES)
 	
-	return _convert_functions(editor, functions_locations)
+	return _convert_data_types(editor, variable_locations)
 
 
 static func _convert_functions(editor: CodeEdit, functions_locations: Array[Vector2i]):
 	var converted_functions: Array[String]
+
 	for function_location in functions_locations:
-		var current_line = editor.get_line(function_location.y)
+		var current_line = EditorHelper.remove_comments(editor.get_line(function_location.y))
 		var _return_type = current_line.get_slice(" ", 0)
 		var function_name = current_line.get_slice(" ", 1).get_slice("(", 0)
 		var function_parameters = current_line.get_slice("(", 1).get_slice(")", 0)
@@ -39,3 +40,21 @@ static func _convert_functions(editor: CodeEdit, functions_locations: Array[Vect
 		converted_functions.append(converted_function)
 	
 	return converted_functions
+
+
+static func _convert_data_types(editor: CodeEdit, data_types_locations: Array[Vector2i]):
+	var converted_data_types: Array[String]
+	var converted_variable 
+	for data_type_location in data_types_locations:
+		var current_line = EditorHelper.remove_comments(editor.get_line(data_type_location.y))
+		var variable_name = current_line.get_slice(" ", 1).get_slice("=", 0)
+		var variable_value = current_line.get_slice("=", 1).replace(";", "")
+		
+		if variable_value.is_empty():
+			converted_variable = "var %s;" % [variable_name]
+		else:
+			converted_variable = "var %s = %s;" % [variable_name, variable_value]
+		
+		converted_data_types.append(converted_variable)
+	
+	return converted_data_types
